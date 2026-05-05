42de77f879d4905d19d890308c7a29d50ea1cb9d
pavo skocibusic <pavo.skocibusic@gmail.com>
2026-05-05 18:30:51 +0200

    modified the base files - added remote images in next config for now - adding full project css - updaing initial layout main component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.base44.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com", // Dodajemo odmah i Unsplash za proizvode
+      },
+    ],
+  },
 };
 
 export default nextConfig;
